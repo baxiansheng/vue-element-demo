d054f0dca3c1c95a386d9c03fa9b64ae9c083535
baxiansheng <1569561258@qq.com>
2022-03-10 17:17:31 +0800

    2.0

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { login } from "@/api/auth.js";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userInfo: {
+      username: '',
+      token: '',
+      icon: '',
+    },
+    routers: []
   },
   mutations: {
+    setUserInfo (state, data) {
+      state.userInfo = data
+    }
   },
   actions: {
+    login ({ commit }, data) {
+      login(data).then((res)=>{
+        // 根据响应，执行操作
+        commit('setUserInfo', res.data)
+      })
+    }
   },
   modules: {
   }
